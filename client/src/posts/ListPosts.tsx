e59fbd76851d5f190dcbe3ee68cf8172882f6e63
unknown <ohndaniel@gmail.com>
2022-11-09 22:15:59 -0700

    Update client with Posts type for Typescript.

--- client/src/posts/ListPosts.tsx
+++ client/src/posts/ListPosts.tsx
@@ -1,8 +1,37 @@
+import { useState } from "react"
+import { EmptyStatement } from "typescript"
+
+// Interface is only for Object/Classes
+// interface Posts {
+//     posts: Array<string>
+// }
+
+type Posts = Array<string>
 
 const ListPosts = () => {
+    let [postsData, setPostsData] = useState<Posts | []>([])
+
+    const getPosts = async () => {
+        try {
+            const res = await fetch("/posts")
+            const data = await res.json()
+
+            setPostsData(data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <>
             <h4>List Posts</h4>
+            {postsData ? 
+                postsData.map((post: string) => {
+                    return (
+                        <p>Testing</p>
+                    )
+                })
+             : <p>No posts</p>}
         </>
     )
 }
