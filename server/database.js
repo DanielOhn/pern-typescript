fbd8a66594a6d93121a1728ba4cde584d9981b47
DanielOhn <ohndaniel@gmail.com>
2022-11-14 14:53:31 -0700

    Update database.ts and server.ts, they now work and pull posts from the db.

--- server/database.js
+++ server/database.js
@@ -1,14 +1,13 @@
-import { Pool } from 'pg'
+'use strict'
+exports.__esModule = true
+var pg_1 = require('pg')
 require('dotenv').config()
-
-const devConfig = {
+var devConfig = {
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
 }
-
-const pool = new Pool(devConfig)
-
-export default pool
+var pool = new pg_1.Pool(devConfig)
+exports['default'] = pool
--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,34 @@
-import * as express from "express"
-import pool from "./database"
+import * as express from 'express'
+import pool from './database'
 
 const app = express()
-const port = 3000
+const port = 8000
 
 // GET posts
-app.post("/posts", async (req, res) => {
-    try {
-        const { text } = req.body;
-        const newPost = await pool.query("INSERT INTO posts (text) VALUES ($1) RETURNING *", [text])
-
-        res.json(newPost.rows[0])
-    }
+app.get('/posts', async (req, res) => {
+  try {
+    const allPosts = await pool.query('SELECT * FROM posts')
+    res.json(allPosts.rows)
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 // CREATE posts
+app.post('/posts', async (req, res) => {
+  try {
+    const { text } = req.body
+    const newPost = await pool.query(
+      'INSERT INTO posts (text) VALUES ($1) RETURNING *',
+      [text]
+    )
 
-app.get("/", (req, res) => {
-    res.send("Hello")
+    res.json(newPost.rows[0])
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 app.listen(port, () => {
-    console.log(`Running server on http://localhost:${port}`)
+  console.log(`Running server on http://localhost:${port}`)
 })
